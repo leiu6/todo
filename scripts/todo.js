6a17a42f36338d2d1f120de9cc27084049ab8920
Leighton Mitchell <ltmitchell2000@gmail.com>
2021-04-27 02:03:30 -0500

    Added some more rudimentary html to manipulate with the javascript. Also added the object for the list into the javascript.

---
+++ scripts/todo.js
@@ -0,0 +1,11 @@
+//This is a simple to-do script.
+//
+//This program will have cool features.
+//
+//Types of fields:
+//"name": the name of the list item
+//"description": an optional short description of the list item
+//"duedate": the date the item is due
+//"subtasks": an array of further subtasks to be accomplished
+
+let list = new Object(); //define a new object for the list
