b3f6acc0c8311274afeb34fa8bee70cc68a00f73
Leighton Mitchell <ltmitchell2000@gmail.com>
2021-04-27 12:05:33 -0500

    Refactored javascript code and wrote in better list entries. Still need to fix the checkbox system for list entries however.

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,46 +1,56 @@
-//This is a simple to-do script.
-//
-//This program will have cool features.
-//
-//Types of fields:
-//"name": the name of the list item
-//"description": an optional short description of the list item
-//"duedate": the date the item is due
-//"done" boolean value that says whether the task is done
+let list = new Array(); //define array of objects for list
 
-let list = new Array(); //define a new object for the list
+const taskName = document.querySelector('#name'); //input for task name
+const taskDescription = document.querySelector('#description');
+const taskDate = document.querySelector('#duedate');
+const taskDone = document.querySelector('#done');
 
-const taskName = document.querySelector('#name'); //get object for text box
+const pageList = document.querySelector('#list'); //the area where the list is on the page
 
-const taskDescription = document.querySelector('#description'); //get object for description box
+const taskAdd = document.querySelector('#add');
 
-const taskAdd = document.querySelector('#add'); //get the button to add tasks
+taskAdd.onclick = () => {
+    addToList(taskName.value, taskDescription.value, taskDate.value, taskDone.checked);
+    syncListToPage();
+};
 
-const pageList = document.querySelector('#list'); //get the <ul> where the list is contained
+//this function syncs the page DOM and the list object
+function syncListToPage() {
+    pageList.innerHTML = ''; //reset contents of ul to redraw
 
-taskAdd.onclick = getInput;
-
-function getInput() {
-    addEntry(taskName.value, taskDescription.value, '11/25/21', true);
-    syncLists();
-}
-
-function addEntry(name, description, duedate, done) {
-    let entry = {
-        "name": name,
-        "description": description,
-        "duedate": duedate,
-        "done": done
-    };
-    list.unshift(entry);
-}
-
-function syncLists() {
-    pageList.innerHTML = ''; //remove content of page list to re populate
     for (let i = 0; i < list.length; i++) {
-        let listItem = document.createElement("LI");
-        listItem.innerHTML = list[i]["name"] + list[i]["description"] + list[i]["duedate"] + list[i]["done"];
+        let listEntry = document.createElement('LI'); //create the li where the list entry lies
 
-        pageList.appendChild(listItem);
+        let checkedString = '';
+        if (list[i].done == true) {
+            checkString = 'checked';
+        }
+
+        listEntry.innerHTML = `
+            <div class="check">
+                <input type="checkbox" name="task${i}" id="task${i}" ${checkedString}>
+            </div>
+            <div class="name"><span>${list[i].name}</span></div>
+            <div class="description">
+                <p>
+                    ${list[i].description}
+                </p>
+            </div>
+            <div class="duedate"><span>${list[i].duedate}</span></div>
+        `;
+
+        pageList.appendChild(listEntry); //add the list entry to the page
     }
+}
+
+//add a user entry to the list array
+function addToList(name, description, date, done) {
+    let entry = {
+        'name': name,
+        'description': description,
+        'date': date,
+        'done': done
+    };
+
+    list.unshift(entry);
 }
