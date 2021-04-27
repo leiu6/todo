0076636228a18c85d09e1ba095500a4b8bf10201
Leighton Mitchell <ltmitchell2000@gmail.com>
2021-04-27 16:17:56 -0500

    Did slight refactoring and commenting for javascript

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,18 +1,17 @@
 let list = new Array(); //define array of objects for list
 
 const taskName = document.querySelector('#name'); //input for task name
-const taskDate = document.querySelector('#duedate');
-
+const taskDate = document.querySelector('#duedate'); //input for task due date
+const taskAdd = document.querySelector('#add'); //button to add the task to the list
 const pageList = document.querySelector('#list'); //the area where the list is on the page
+const headerText = document.querySelector('.header-text'); //the <span></span> where the header's text is located
 
-const taskAdd = document.querySelector('#add');
-
-const headerText = document.querySelector('.header-text');
-
+//Add inital content to the header
 let header = document.createElement('h1');
 header.textContent = 'my list';
 headerText.appendChild(header);
 
+//Add text box for renaming the list if the header is clicked
 headerText.onclick = () => {
     headerText.innerHTML = `
         <input type="text" id="headerInput" name="headerInput">
@@ -34,8 +33,9 @@ headerText.onclick = () => {
     });
 }
 
+//onclick event for if the task add button is clicked
 taskAdd.onclick = () => {
-    addToList(taskName.value, taskDate.value, false);
+    addToList(taskName.value, taskDate.value, false); //task completion is always marked false for the first time, because you would not be putting it in the list if it was completed.
     syncListToPage();
 };
 
@@ -77,6 +77,7 @@ function addToList(name, date, done) {
     list.unshift(entry);
 }
 
+//function from stack overflow that allows cross browser focus triggering. Use to trigger focus to the textbox that appears whenever the header is clicked
 function triggerFocus(element) {
     var eventType = "onfocusin" in element ? "focusin" : "focus",
         bubbles = "onfocusin" in element,
